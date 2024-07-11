8e5335c34b64ec56432f1cfad839f7ad4a1dc853
Randomy <154236134+Randomy66@users.noreply.github.com>
2024-07-11 23:25:39 +0630

    Add files via upload

--- Javascript/invite.js
+++ Javascript/invite.js
@@ -10,4 +10,28 @@ function copyToClipboard() {
     document.execCommand("copy");
   
   }
+function QR() {
+    let x = document.getElementById("QR");
+    let src = x.src;
+
+    if (src.includes("Resources/QR_2.png")) {
+        x.src = "Resources/QR_3.png";
+    } 
+    else if (src.includes("Resources/QR_3.png")) {
+        x.src = "Resources/QR_4.png";
+    } 
+    else if (src.includes("Resources/QR_4.png")) {
+        x.src = "Resources/QR_2.png";
+    }
+    else{
+      x.src = "Resources/QR_2.png";
+      document.getElementById("ch_QR").innerHTML = "QR code doesn't work?";
+    }
+
+}
+function ch_QR() {
+  document.getElementById("ch_QR").innerHTML = "Try the simple QR code. If this is not still working, click Go Now! to go to website";
+  document.getElementById("QR").src = "Resources/QRcode.jpg"
+}
+
   
